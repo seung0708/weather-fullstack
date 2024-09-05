ee1caff31bc31143ad5b3b502f6fd555ab9c08c2
Seung Hyun Kim <seung.kim0708@gmail.com>
2024-09-05 16:05:45 -0700

    styling the login form

--- frontend/src/components/register/Register.js
+++ frontend/src/components/register/Register.js
@@ -2,35 +2,7 @@ import React from 'react'
 
 const Register = () => {
   return (
-    <section id='register' className='container'>
-        <form>
-            <label>
-                Name: 
-                <input type='text' />
-            </label>
-            <br />
-            <label>
-                Email: 
-                <input type='email' />
-            </label>
-            <br />
-            <label>
-                City: 
-                <input />
-            </label>
-            <br />
-            <label>
-                Password
-                <input />
-            </label>
-            <br />
-            <label>
-                Confirm Password: 
-                <input />
-            </label>
-            <br />
-            <button type='submit'>Submit</button>
-        </form>
+    <section>
     </section>
   )
 }
